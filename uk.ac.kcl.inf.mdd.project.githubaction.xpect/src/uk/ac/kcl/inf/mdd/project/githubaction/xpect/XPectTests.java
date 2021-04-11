eb194601dfce83d52e4deb03b150743ef75ce5d9
Part4234 <55008455+part4234@users.noreply.github.com>
2021-04-11 23:30:47 +0100

    organise xpect imports

--- uk.ac.kcl.inf.mdd.project.githubaction.xpect/src/uk/ac/kcl/inf/mdd/project/githubaction/xpect/XPectTests.java
+++ uk.ac.kcl.inf.mdd.project.githubaction.xpect/src/uk/ac/kcl/inf/mdd/project/githubaction/xpect/XPectTests.java
@@ -1,16 +1,13 @@
 package uk.ac.kcl.inf.mdd.project.githubaction.xpect;
 
 import org.eclipse.xpect.runner.XpectRunner;
-import org.eclipse.xpect.xtext.lib.tests.XtextTests;
-import org.junit.runner.RunWith;
-import org.eclipse.xpect.runner.XpectRunner; 
-import org.eclipse.xpect.runner.XpectSuiteClasses; 
+import org.eclipse.xpect.runner.XpectSuiteClasses;
 import org.eclipse.xpect.xtext.lib.tests.GeneratorTest;
-import org.eclipse.xpect.xtext.lib.tests.JvmModelInferrerTest; 
-import org.eclipse.xpect.xtext.lib.tests.LinkingTest; 
-import org.eclipse.xpect.xtext.lib.tests.ResourceDescriptionTest; 
-import org.eclipse.xpect.xtext.lib.tests.ScopingTest; 
-import org.eclipse.xpect.xtext.lib.tests.ValidationTest; 
+import org.eclipse.xpect.xtext.lib.tests.JvmModelInferrerTest;
+import org.eclipse.xpect.xtext.lib.tests.LinkingTest;
+import org.eclipse.xpect.xtext.lib.tests.ResourceDescriptionTest;
+import org.eclipse.xpect.xtext.lib.tests.ScopingTest;
+import org.eclipse.xpect.xtext.lib.tests.ValidationTest;
 import org.junit.runner.RunWith; 
 
 
